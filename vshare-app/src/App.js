abe594f09def985def5b54fe89803f241bac51f3
alekseiapa <aapaseev@bk.ru>
2022-06-18 00:00:10 +0800

    feat: added grid system

--- vshare-app/src/App.js
+++ vshare-app/src/App.js
@@ -1,8 +1,22 @@
+import { Grid } from "@material-ui/core";
+
 import youtube from "./services/yt-api";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Grid style={{ justifyContent: "center" }} container spacing={10}>
+        <Grid item xs={11}>
+          <Grid spacing={10}>
+            <Grid item xs={12}></Grid>
+            <Grid item xs={8}></Grid>
+            <Grid item xs={4}></Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
